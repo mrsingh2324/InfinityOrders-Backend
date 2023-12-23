5a5964c6fe2b6a00a5ef6792b7b6f5fd92ce0182
Satyam Singh <87685197+mrsingh2324@users.noreply.github.com>
2023-12-23 17:20:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: "https://www.infinityorders.live/",
+  origin: "https://www.infinityorders.live",
   credentials: false
 }));
 
